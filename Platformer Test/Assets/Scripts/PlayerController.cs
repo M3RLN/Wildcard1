60e113b5346a5f1d420a14dfae930eeb4222be91
Slendolan <32447218+Slendolan@users.noreply.github.com>
2017-11-28 16:19:01 -0800

    Changed some stuff
    Added and removed some things

--- Platformer Test/Assets/Scripts/PlayerController.cs
+++ Platformer Test/Assets/Scripts/PlayerController.cs
@@ -7,14 +7,39 @@ public class PlayerController : MonoBehaviour {
     public float moveSpeed;
     public float jumpHeight;
 
+    Animator theanimator;
+    public bool isMoving;
+    public bool isJumping;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+
+    // Use this for initialization
+    void Start () {
+        //theanimator = gameObject.GetComponent<Animator>();
+        theanimator = GetComponent<Animator>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
+
+        isMoving = false;
+        isJumping = false;
+
+        if (GetComponent<Rigidbody2D>().velocity.x != 0)
+            isMoving = true;
+        else
+            isMoving = false;
+        
+
+
+        if (GetComponent<Rigidbody2D>().velocity.y > 0)
+            isJumping = true;
+        else
+            isJumping = false;
+        
+
+        theanimator.SetBool("mve", isMoving);
+        theanimator.SetBool("jmp", isJumping);
+
 		if(Input.GetKeyDown (KeyCode.Space))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
@@ -30,4 +55,6 @@ public class PlayerController : MonoBehaviour {
             GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
         }
     }
+    
 }
+
